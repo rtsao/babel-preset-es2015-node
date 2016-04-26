a9b6ebc11101dd1ef0cd36ec3d470c9beea07ad3
Ryan Tsao <rtsao@uber.com>
2016-04-26 22:31:31 +0200

    Added shorthand properties

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
     require("babel-plugin-transform-es2015-function-name"),
     require("babel-plugin-transform-es2015-modules-commonjs"),
     require("babel-plugin-transform-es2015-parameters"),
+    require("babel-plugin-transform-es2015-shorthand-properties"),
     require("babel-plugin-transform-es2015-sticky-regex"),
     require("babel-plugin-transform-es2015-unicode-regex"),
   ]
